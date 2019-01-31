9aed11e134aa5fd381efc2a6b9bed0730606e2cb
Augusto Rucle <augustorucle@gmail.com>
2019-01-30 19:21:24 -0600

    First part application

---
+++ src/app/Pages/login/login.page.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  public userForm : FormGroup;
+  
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
+    this.userForm = this.formBuilder.group({
+      'email': [''],
+      'password': [''],
+    });
+   }
+
+  ngOnInit() {
+  }
+
+  loginForm(){
+    console.log(this.userForm.value)
+    this.router.navigateByUrl('/menu/tabs/tab1');
+  }
+}
